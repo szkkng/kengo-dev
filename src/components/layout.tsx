fa75709ff65e589f69f71e4ac52b1d966af88bdf
Kengo Suzuki <git@kengo.dev>
2021-09-20 22:25:27 +0900

    added site description

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,13 +13,7 @@ export const Layout: React.FC<Props> = ({ children, home }) => {
     <div className='flex flex-col min-h-screen'>
       <Head>
         <link rel='icon' href='/images/favicon.png' />
-        <meta name='description' content='This is my tech blog.' />
-        <meta
-          property='og:image'
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta name='description' content="suzuki kengo's blog" />
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
