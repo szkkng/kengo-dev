f4b04085ad8f89a9beda0f3b1a2649caa6605347
Kengo Suzuki <git@kengo.dev>
2021-10-09 17:52:44 +0900

    added GA4 settings

---
+++ src/lib/gtag.ts
@@ -0,0 +1,1 @@
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID ?? '';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import 'highlight.js/styles/nord.css';
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { GA_ID } from 'lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id='analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
