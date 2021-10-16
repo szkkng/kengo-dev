a7c8c7bae00fff5d1b824864b7bde914169f7b29
Kengo Suzuki <git@kengo.dev>
2021-10-16 11:49:56 +0900

    Revert "added_table_of_contents_#2"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }: InferGetServerSidePropsType<typeo
   return (
     <Layout home>
       <Seo pageTitle={siteTitle} />
-      <div className='w-11/12 md:w-10/12 m-auto mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      <div className='w-11/12 md:w-10/12 m-auto grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {allPostsData.map(({ id, date, title, thumbnail }) => (
           <div
             key={id}
