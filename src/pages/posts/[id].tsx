8909149d0d644d2bb54107892cdb7bc364589144
Kengo Suzuki <git@kengo.dev>
2022-08-16 20:54:06 +0900

    Add comment section #62

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, InferGetServerSidePropsType } from 'next';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { MdUpdate } from 'react-icons/md';
+import { Comments } from 'components/Comments';
 import { Date } from 'components/Date';
 import { Layout } from 'components/Layout';
 import Seo from 'components/Seo';
@@ -41,6 +42,9 @@ export default function Post({ postData }: InferGetServerSidePropsType<typeof ge
       <div className='flex justify-center'>
         <article className='w-11/12 lg:w-3/5 xl:w-1/2 xl:ml-16'>
           <div className='post' dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          <div className='mt-10'>
+            <Comments />
+          </div>
         </article>
         <div className='hidden break-words lg:block lg:w-1/4 lg:ml-10 mt-16'>
           <div className='sticky top-32 overflow-auto h-600'>
