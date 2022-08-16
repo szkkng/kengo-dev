2d096b1b2eb9009894a31318f72ed1e66a2ce895
szkkng <zukkesukke23@gmail.com>
2022-08-16 20:54:06 +0900

    Add comment section #62

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout: React.FC<Props> = ({ children, home }) => {
       <main className='container m-auto'>{children}</main>
       <footer className='h-20'>
         {!home && (
-          <div className='mt-20 pt-4 text-center'>
+          <div className='mt-10 pt-4 text-center'>
             <Link href='/'>
               <a className='text-cream text-lg'>Home</a>
             </Link>
