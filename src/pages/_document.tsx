97dce7f1c293ef310967b0e86930923e26539341
szkkng <zukkesukke23@gmail.com>
2023-04-26 10:31:07 +0900

    Add lang attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
