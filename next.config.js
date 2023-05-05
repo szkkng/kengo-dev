1512b1cd47737b44803dc0e189ed3be47403fc44
Kengo Suzuki <git@kengo.dev>
2023-05-05 11:34:04 +0900

    Add app dir config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
 };
+
+module.exports = nextConfig;
