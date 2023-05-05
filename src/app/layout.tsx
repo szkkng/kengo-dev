065fa32f38b32f526b92db32a5cb93f437a944fb
Kengo Suzuki <git@kengo.dev>
2023-05-05 18:55:32 +0900

    Migrate pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import 'highlight.js/styles/github-dark.css';
+import 'styles/globals.css';
+
+export const metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+};
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
