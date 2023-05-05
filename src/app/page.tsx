bca3d493e0b071a561c6c7483711491cf5a20ffd
Kengo Suzuki <git@kengo.dev>
2023-05-05 23:23:37 +0900

    Remove layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,34 @@ import Link from 'next/link';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { MdUpdate } from 'react-icons/md';
 import Date from '@/components/Date';
-import Layout from '@/components/Layout';
 import { getSortedPostsData } from '@/lib/posts';
 
 const Home = async () => {
   const allPostsData = getSortedPostsData();
 
   return (
-    <Layout home>
-      <div className='w-11/12 m-auto mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {allPostsData.map(({ id, createdDate, updatedDate, title, thumbnail }) => (
-          <div
-            key={id}
-            className='flex flex-col border transition delay-75 border-darkGrey hover:border-cyan hover:border-opacity-50'
+    <div className='w-11/12 m-auto mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {allPostsData.map(({ id, createdDate, updatedDate, title, thumbnail }) => (
+        <div
+          key={id}
+          className='flex flex-col border transition delay-75 border-darkGrey hover:border-cyan hover:border-opacity-50'
+        >
+          <Link
+            href={`/posts/${encodeURIComponent(id)}`}
+            className='flex flex-col justify-between text-cream mt-2 p-5 w-full h-full transition delay-75 hover:text-cyan'
           >
-            <Link
-              href={`/posts/${encodeURIComponent(id)}`}
-              className='flex flex-col justify-between text-cream mt-2 p-5 w-full h-full transition delay-75 hover:text-cyan'
-            >
-              <Image src={thumbnail} width={840} height={540} objectFit='contain' alt='' />
-              <div className='text-center text-xl font-bold mt-2'>{title}</div>
-              <div className='flex items-center justify-center  text-midGrey mt-3 text-sm'>
-                <MdUpdate className='mr-1 text-base' />
-                <Date dateString={updatedDate} />
-                <AiOutlineCalendar className='ml-5 mr-1' />
-                <Date dateString={createdDate} />
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </Layout>
+            <Image src={thumbnail} width={840} height={540} objectFit='contain' alt='' />
+            <div className='text-center text-xl font-bold mt-2'>{title}</div>
+            <div className='flex items-center justify-center  text-midGrey mt-3 text-sm'>
+              <MdUpdate className='mr-1 text-base' />
+              <Date dateString={updatedDate} />
+              <AiOutlineCalendar className='ml-5 mr-1' />
+              <Date dateString={createdDate} />
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
   );
 };
 
