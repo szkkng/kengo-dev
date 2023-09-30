a3f1dfa5ebd3107fc7663fde65147e3798954ac2
szkkng <zukkesukke23@gmail.com>
2023-09-30 21:25:15 +0900

    Fix eslint errors

--- app/_components/date.tsx
+++ app/_components/date.tsx
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns';
 
-type Props = { dateString: string };
+interface Props { dateString: string }
 
 const Date = ({ dateString }: Props) => {
   const date = parseISO(dateString);
