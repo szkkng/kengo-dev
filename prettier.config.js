62b2bdceb590a6d3a8d79b0ecc79e7a0d23d173f
szkkng <zukkesukke23@gmail.com>
2023-09-30 10:04:55 +0900

    Add prettier-plugin-tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
