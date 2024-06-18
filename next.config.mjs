241fd40d6128fe09172469dbb6550c8b1c2e29ba
szkkng <hi@kengo.dev>
2024-06-18 14:45:23 +0900

    Setup next mdx

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
