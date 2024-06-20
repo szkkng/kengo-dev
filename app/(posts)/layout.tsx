be0eeb7556fdfaba5e61694bcadb0dd14a276744
szkkng <hi@kengo.dev>
2024-06-20 17:34:08 +0900

    Migrate to astro components

--- app/(posts)/layout.tsx
+++
@@ -1,5 +0,0 @@
-const Layout = async ({ children }: { children: React.ReactNode }) => {
-  return <article className='prose mt-6'>{children}</article>;
-};
-
-export default Layout;
--- app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { type Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Not Found',
-};
-
-const NotFound = () => <h1 className='mt-6 text-cream'>Page Not Found</h1>;
-
-export default NotFound;
