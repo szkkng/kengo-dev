162fa76fb9368cceaf17d212db5d904df60615a5
szkkng <hi@kengo.dev>
2024-06-20 18:00:00 +0900

    Add rehype pretty code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from 'rehype-pretty-code';
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  keepBackground: false,
+}
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, options]]
+  },
   integrations: [tailwind(), mdx()]
 });
