86f82466220e3217394696da09c2e9a8f03b6d9d
Kengo Suzuki <git@kengo.dev>
2025-11-30 04:51:28 +0900

    Add devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs allSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
+            packages = with pkgs; [
+              bun
+              nodejs
+              typescript-language-server
+            ];
+          };
+        }
+      );
+    };
+}
